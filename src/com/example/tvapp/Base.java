e2ee077688154854ab0e98c23db3a6c891e14199
Lucas Martins <lucasilvamartins@gmail.com>
2015-04-13 20:33:22 +0100

    Simple http get request to the web service
    Signed-off-by: Lucas Martins <lucasilvamartins@gmail.com>

--- src/com/example/tvapp/Base.java
+++ src/com/example/tvapp/Base.java
@@ -5,4 +5,3 @@ import android.app.Activity;
 public class Base extends Activity {
 
 }
-
--- src/com/example/tvapp/LoginActivity.java
+++ src/com/example/tvapp/LoginActivity.java
@@ -1,8 +1,23 @@
 package com.example.tvapp;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.params.BasicHttpParams;
+import org.apache.http.params.HttpConnectionParams;
+import org.apache.http.params.HttpParams;
+
 import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -13,6 +28,8 @@ public class LoginActivity extends Activity {
 
 	private EditText user;
 	private EditText password;
+	private static HttpParams 			httpParameters;
+	private static DefaultHttpClient 	httpClient;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +38,30 @@ public class LoginActivity extends Activity {
 		
 		user = (EditText) findViewById(R.id.user);
 		password = (EditText) findViewById(R.id.password);
+		
+		
+		/*HttpClient client = new DefaultHttpClient();
+		HttpGet request = new HttpGet("http://api-tv-program.herokuapp.com/get-tv-program/MDO/13-04-2015");
+
+		HttpResponse response;
+		try {
+			response = client.execute(request);
+
+			Log.d("Response of GET request", response.toString());
+		} catch (ClientProtocolException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}*/
+		Log.v("teste", "ok");
+		httpParameters = new BasicHttpParams();
+		HttpConnectionParams.setConnectionTimeout(httpParameters,
+				10000);
+		HttpConnectionParams.setSoTimeout(httpParameters, 20000);
+		httpClient = new DefaultHttpClient(httpParameters);
+		
+		new makeGetRequest(this).execute("http://api-tv-program.herokuapp.com/get-tv-program/MDO/13-04-2015");
+		
 	}
 
 	@Override
@@ -51,6 +92,82 @@ public class LoginActivity extends Activity {
 		} else {
 			String errorMessage = getString(R.string.auth_error);
 			Toast.makeText(this, errorMessage, Toast.LENGTH_SHORT).show();
+		}
+	}
+
+	private class makeGetRequest extends AsyncTask<String, Void, String> {
+
+		protected ProgressDialog 		dialog;
+		protected Context 				context;
+
+		public makeGetRequest(Context context)
+		{
+			this.context = context;
+		}
+		
+		@Override
+		protected void onPreExecute() {
+			super.onPreExecute();	
+			this.dialog = new ProgressDialog(context, 1);
+			this.dialog.setMessage("Retrieving Donations List");
+			this.dialog.show();
+		}
+
+		@Override
+		protected void onPostExecute(String result) {
+			super.onPostExecute(result);
+			
+			if (dialog.isShowing())
+				dialog.dismiss();
+		}
+
+		@Override
+		protected String doInBackground(String... params) {
+			
+			String result = "";
+			try {
+				HttpGet getRequest = new HttpGet("http://api-tv-program.herokuapp.com/get-tv-program/MDO/13-04-2015");
+				getRequest.setHeader("accept", "application/json");
+				//getRequest.setHeader("accept","text/plain");
+				HttpResponse response = httpClient.execute(getRequest);
+				result = getResult(response).toString();
+				Log.v("Response of GET request", result);
+				
+			} catch (Exception e) {
+				Log.v("TVAPP","ASYNC ERROR" + e.getMessage());
+			}
+			return result;
+			
+			
+			
+			/**
+			HttpClient client = new DefaultHttpClient();
+			HttpGet request = new HttpGet("http://api-tv-program.herokuapp.com/get-tv-program/MDO/13-04-2015");
+
+			HttpResponse response;
+			try {
+				response = client.execute(request);
+
+				Log.d("Response of GET request", response.toString());
+			} catch (ClientProtocolException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return null;
+			**/
+		}
+		
+		private StringBuilder getResult(HttpResponse response)
+				throws IllegalStateException, IOException {
+			StringBuilder result = new StringBuilder();
+			BufferedReader br = new BufferedReader(new InputStreamReader(
+					(response.getEntity().getContent())), 1024);
+			String output;
+			while ((output = br.readLine()) != null)
+				result.append(output);
+
+			return result;
 		}
 	}
 }
